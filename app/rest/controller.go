1f59f49911482d5dfa2177c0cd3f037e3678fe89
Norbert Csibra <norbert.csibra@proemergotech.com>
2019-12-03 10:47:38 +0000

    Add metrics endpoint and debug pprof options

--- app/rest/controller.go
+++ app/rest/controller.go
@@ -2,9 +2,12 @@ package rest
 
 import (
 	"net/http"
+	"net/http/pprof"
+	"runtime"
 
 	"github.com/labstack/echo"
 	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gitlab.com/proemergotech/dliver-project-skeleton/app/service"
 	"gitlab.com/proemergotech/dliver-project-skeleton/app/validation"
 	log "gitlab.com/proemergotech/log-go"
@@ -15,23 +18,38 @@ import (
 type Controller struct {
 	echoEngine *echo.Echo
 	svc        *service.Service
+	debugPProf bool
 }
 
 func NewController(
 	echoEngine *echo.Echo,
 	svc *service.Service,
+	debugPProf bool,
 ) *Controller {
 	return &Controller{
 		echoEngine: echoEngine,
 		svc:        svc,
+		debugPProf: debugPProf,
 	}
 }
 
 func (c *Controller) start() {
+	if c.debugPProf {
+		runtime.SetBlockProfileRate(1)
+		runtime.SetMutexProfileFraction(5)
+
+		c.echoEngine.GET("/debug/pprof/*", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
+		c.echoEngine.GET("/debug/pprof/cmdline", echo.WrapHandler(http.HandlerFunc(pprof.Cmdline)))
+		c.echoEngine.GET("/debug/pprof/profile", echo.WrapHandler(http.HandlerFunc(pprof.Profile)))
+		c.echoEngine.GET("/debug/pprof/symbol", echo.WrapHandler(http.HandlerFunc(pprof.Symbol)))
+		c.echoEngine.GET("/debug/pprof/trace", echo.WrapHandler(http.HandlerFunc(pprof.Trace)))
+	}
 
 	c.echoEngine.Add(http.MethodGet, "/healthcheck", func(eCtx echo.Context) error {
 		return eCtx.String(http.StatusOK, "ok")
 	})
+
+	c.echoEngine.GET("metrics", echo.WrapHandler(promhttp.Handler()))
 
 	apiRoutes := c.echoEngine.Group("/api/v1")
 	apiRoutes.Use(echolog.DebugMiddleware(log.GlobalLogger(), true, true))
